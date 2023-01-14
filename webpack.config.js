d3af73cd9068e4138e3f9669202faf16929e4082
Tegas <29030328+Tegas@users.noreply.github.com>
2023-01-13 19:08:45 -0500

    Adding support for react

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.ts',
-  devtool: 'inline-source-map',
+  entry: './src/index.tsx',
+  devtool: 'source-map',
+  mode: 'production',
   module: {
     rules: [
       {
@@ -19,4 +21,9 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Outlook'
+    })
+  ],
 };
