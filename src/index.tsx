9428061e1431326b1947283f6a54179f5ea92450
tegas <29030328+Tegas@users.noreply.github.com>
2023-01-15 17:15:11 -0500

    Adding Pure CSS library

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import 'purecss/build/pure.css'
 import './style.css';
 
 function App() {
-  return <div>Hello World</div>;
+	return (
+		<div className="pure-g">
+			<div className="pure-u-1-3">
+				<div className='box'>
+					Column A
+				</div>
+			</div>
+			<div className="pure-u-1-3">
+				<div className='box'>
+					Column B
+				</div>
+			</div>
+			<div className="pure-u-1-3">
+				<div className='box'>
+					Column C
+				</div>
+			</div>
+		</div>
+	);
 }
 
 const root = createRoot(document.body);
