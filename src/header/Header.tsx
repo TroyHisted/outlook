741a70abf4e4a016768ab744d6c9d02f80d9822d
tegas <29030328+Tegas@users.noreply.github.com>
2023-07-30 18:16:38 -0400

    Calculate date

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,36 +1,40 @@
-export default () => (
-	<header>
-		<nav>
-			<ul>
-				<li>
-					<strong>
-						<a href="#">
-							<img
-								src="favicon.ico"
-								width={48}
-								alt="Logo containing the letters O and L"
-							/>
-						</a>
-					</strong>
-				</li>
-			</ul>
-			<ul>
-				<li>
-					<a href="#/news">News</a>
-				</li>
-				<li>
-					<a href="#/weather">Weather</a>
-				</li>
-				<li>
-					<a href="#/sports">Sports</a>
-				</li>
-			</ul>
-		</nav>
-		<div>
-			Outlook -{' '}
-			<small>
-				February 2<sup>nd</sup> 2020
-			</small>
-		</div>
-	</header>
-);
+export default () => {
+	const formattedDate = new Date().toLocaleDateString('en-us', {
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric',
+	});
+
+	return (
+		<header>
+			<nav>
+				<ul>
+					<li>
+						<strong>
+							<a href="#">
+								<img
+									src="favicon.ico"
+									alt="Logo containing the letters O and L"
+								/>
+							</a>
+						</strong>
+					</li>
+				</ul>
+				<ul>
+					<li>
+						<a href="#/news">News</a>
+					</li>
+					<li>
+						<a href="#/weather">Weather</a>
+					</li>
+					<li>
+						<a href="#/sports">Sports</a>
+					</li>
+				</ul>
+			</nav>
+			<div>
+				Outlook - <small>{formattedDate}</small>
+			</div>
+		</header>
+	);
+};
