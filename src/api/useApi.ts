988f6a0bc432eb946be78cd31714302d2a5d3067
Tegas <29030328+Tegas@users.noreply.github.com>
2023-08-08 12:57:52 -0400

    Swapping to useApi

--- src/api/useApi.ts
+++ src/api/useApi.ts
@@ -1,26 +1,47 @@
 import { useState } from 'react';
 import { LoadingStatus } from '../page/weather/types/LoadingStatus';
 
-interface ApiResponse<Data, Params> {
+/**
+ * Represents an API that will return a piece of json data.
+ */
+interface Api<Data, Params> {
+	// The data returned from the api call
 	data: Data;
+	//
 	loadingStatus: LoadingStatus;
+	// Contains a generic error message when the most recent api call fails to return a 200 response.
 	error: string;
+	// Initiates a fetch request using the 'get' http method using the api url.
 	get: (query?: Params) => Promise<Data>;
+	// Initiates a fetch request using the 'get' http method against the specified url.
+	getUrl: (url: string, query?: Params) => Promise<Data>;
 }
 
+/**
+ *
+ * @param url - The fully qualified url to the api service endpoint.
+ * @param init - Fetch settings that will be used for each request.
+ * @returns {Api} - The api service.
+ */
 function useApi<
 	Response,
 	Params extends Record<string, string> | void = Record<string, string> | void
->(url: string, init?: RequestInit): ApiResponse<Response, Params> {
+>(url?: string, init?: RequestInit): Api<Response, Params> {
 	const [loadingStatus, setLoadingStatus] =
 		useState<LoadingStatus>('NotLoaded');
 	const [data, setData] = useState<Response>();
 	const [error, setError] = useState<string>();
 
-	const get = (params?: Params) => {
+	const get = (params?: Params) => getUrl(url, params);
+
+	/**
+	 * @param url - The api url.
+	 * @param params - Query parameters (optional).
+	 * @returns The fetch promise.
+	 */
+	const getUrl = (url: string, params?: Params) => {
 		setLoadingStatus('Loading');
 		setData({} as Response);
-
 		let requestUrl = url;
 		const searchParams = new URLSearchParams();
 		if (params !== undefined) {
@@ -34,6 +55,7 @@ function useApi<
 				}
 			);
 		}
+		requestUrl = `${requestUrl}?${searchParams.toString()}`;
 		return fetch(requestUrl, init)
 			.then((response) => {
 				if (response.status === 200) {
@@ -60,6 +82,7 @@ function useApi<
 		loadingStatus,
 		error,
 		get,
+		getUrl,
 	};
 }
 
--- src/page/weather/useForecast.ts
+++ src/page/weather/useForecast.ts
@@ -1,42 +1,27 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import type { Forecast } from './types/Forecast';
 import type { Points } from './types/Points';
-import { LoadingStatus } from './types/LoadingStatus';
+import useApi from '../../api/useApi';
 
 // https://weather-gov.github.io/api/general-faqs
 export default function useForecast(props: { points?: Points }) {
-	const [loadingStatus, setLoadingStatus] =
-		useState<LoadingStatus>('NotLoaded');
-	const [error, setError] = useState<string>();
-	const [forecast, setForecast] = useState<Forecast>();
-
-	const fetchForecast = async (points: Points) => {
-		setLoadingStatus('Loading');
-		const forecastUrl = points.properties.forecast;
-		const response = await fetch(`${forecastUrl}?units=us`, {
-			headers: {
-				'Feature-Flags':
-					'forecast_temperature_qv,forecast_wind_speed_qv',
-			},
-		});
-		if (response.status === 200) {
-			const f: Forecast = await response.json();
-			setForecast(f);
-			setLoadingStatus('Loaded');
-			setError(undefined);
-		} else {
-			setLoadingStatus('NotLoaded');
-			setError('Failed lookup');
-		}
-	};
+	const forecastApi = useApi<Forecast, { units: 'us' }>('', {
+		headers: {
+			'Feature-Flags': 'forecast_temperature_qv,forecast_wind_speed_qv',
+		},
+	});
 
 	useEffect(() => {
-		fetchForecast(props.points);
+		if (props.points?.properties?.forecast) {
+			forecastApi.getUrl(props.points.properties.forecast, {
+				units: 'us',
+			});
+		}
 	}, [props.points]);
 
 	return {
-		loadingStatus,
-		error,
-		forecast,
+		loadingStatus: forecastApi.loadingStatus,
+		error: forecastApi.error,
+		forecast: forecastApi.data,
 	};
 }
