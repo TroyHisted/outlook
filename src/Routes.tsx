f2394eed070d77542077d2fe2c2849e981a0906b
Tegas <29030328+Tegas@users.noreply.github.com>
2024-05-27 15:38:40 -0400

    Updated wouter and pico

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,26 +1,13 @@
 import { Route, Switch } from 'wouter';
+import { useHashLocation } from 'wouter/use-hash-location';
 import Weather from './page/weather/Weather';
 import News from './page/news/News';
 import Sports from './page/sports/Sports';
 import Home from './page/home/Home';
 import NotFound from './page/not-found/NotFound';
-import { useEffect, useState } from 'react';
-
-const getLocationFromHash = () => window.location.hash.replace(/^#/, '') || '/';
 
 const App = () => {
-	const [location, setLocation] = useState(getLocationFromHash());
-
-	useEffect(() => {
-		const updateLocation = () => {
-			setLocation(getLocationFromHash());
-		};
-		window.addEventListener('hashchange', updateLocation);
-		return () => {
-			window.removeEventListener('hashchange', updateLocation);
-		};
-	}, []);
-
+	const [location] = useHashLocation();
 	return (
 		<Switch location={location}>
 			<Route path="/weather" component={Weather} />
